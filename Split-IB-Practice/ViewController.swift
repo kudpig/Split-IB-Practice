55b2ad8ca364fe836d46b93982ab313afea26b07
kudpig <shin.kudo.aa@gmail.com>
2021-02-11 21:54:23 +0900

    2つのページ画面遷移まで(NavigationBar)

--- Split-IB-Practice/ViewController.swift
+++ Split-IB-Practice/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBAction func go2Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "goPage2", sender: nil)
+    }
+    
+    
 
 }
 
